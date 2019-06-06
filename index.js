7f445485c7130a79d09d0b4d9c4d50c40b75b953
TylerGlisson <tyler@tglisson.com>
2019-06-06 12:49:18 -0400

    solution to problem

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-export default {
-  //implement me
+class CleverArray extends Array {
+  constructor(...items) {
+    super(...items);
+  }
+
+  //sum method
+  sum() {
+    if(this.length < 1){
+      return NaN;
+    }
+    const sums = this.reduce((a, b) => a +b);
+    return sums;
+  }
+
+  //average method
+  average() {
+    const avg = this.sum()/this.length;
+    return avg;
+  }
+  
 }
+
+// const clev = new CleverArray();
+// console.log('array', clev);
+
+// console.log(clev.sum());
+export default CleverArray
